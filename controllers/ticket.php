d3fd25fe8d7b9164c9c6e0061bfa2ee206c001a6
shimoyatmm <108167099+jonny0625@users.noreply.github.com>
2023-06-09 09:46:25 +0900

    IMPROVE: コメント・スペースの調整
    コメント・スペースの調整

--- controllers/ticket.php
+++ controllers/ticket.php
@@ -6,7 +6,7 @@ class TicketController{
     public $request_body;
 
     function __construct(){
-        $this->url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . mb_substr($_SERVER['SCRIPT_NAME'],0,-9) . basename(__FILE__, ".php")."/";
+        $this->url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . mb_substr($_SERVER['SCRIPT_NAME'], 0, -9) . basename(__FILE__, ".php")."/";
         $this->request_body = json_decode(mb_convert_encoding(file_get_contents('php://input'),"UTF8","ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN"),true);
     }
 
