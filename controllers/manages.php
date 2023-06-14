f815d93c59c90fedf9e0a337ec3018402a64d012
shimoyatmm <108167099+jonny0625@users.noreply.github.com>
2023-06-14 13:44:29 +0900

    IMPROVE: 変数名の変更
    ・t_movie_managesの検索結果を格納する変数名をmanageに変更

--- controllers/manages.php
+++ controllers/manages.php
@@ -16,25 +16,25 @@ class ManagesController extends Controller{
 
     // idで抽出
     private function getById($id):array{
-        $res_movie  = parent::selectSchedulesById(Sql::SelectSchedulesById, $id)[0]; // 映画TBL検索
+        $res_manage  = parent::selectSchedulesById(Sql::SelectSchedulesById, $id)[0]; // 映画TBL検索
         $res_types  = parent::selectById(Sql::SelectTypesById, $id);                // 上映種別TBL検索
         $res_images = parent::selectById(Sql::SelectImagesById, $id);               // 画像TBL検索
 
         // 映画情報と上映種別情報と画像情報の連結
-        $res_movie["movie_types"]       = array();
-        $res_movie["movie_images"]      = array();
-        $res_movie["advertising_time"]  = Config::AdvertisingTime;
+        $res_manage["movie_types"]       = array();
+        $res_manage["movie_images"]      = array();
+        $res_manage["advertising_time"]  = Config::AdvertisingTime;
 
         foreach($res_types as $res_type){
-            array_push( $res_movie["movie_types"], $res_type['name'] );
+            array_push( $res_manage["movie_types"], $res_type['name'] );
         }
 
         foreach($res_images as $res_image){
-            array_push( $res_movie["movie_images"], parent::url_image($res_image['image_url']) );
+            array_push( $res_manage["movie_images"], parent::url_image($res_image['image_url']) );
         }
 
-        if($res_movie){
-            return $res_movie;
+        if($res_manage){
+            return $res_manage;
         }
         else{
             $this->code = 500;
@@ -43,7 +43,7 @@ class ManagesController extends Controller{
     }
 
     
-    // idで抽出
+    // すべて取得
     private function getAll():array{
         $res_manages  = parent::selectSchedulesAll(Sql::SelectSchedules);   // 映画TBL検索
         $res_types  = parent::select(Sql::SelectTypesAll);                  // 上映種別TBL検索
