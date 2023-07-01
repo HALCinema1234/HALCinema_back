8f6e50a753b722b98fb2ba6dcfa021f91bdb1184
shimoyatmm <108167099+jonny0625@users.noreply.github.com>
2023-07-01 13:54:48 +0900

    FIX: 映画情報の取得方法変更

--- controllers/movies.php
+++ controllers/movies.php
@@ -7,52 +7,24 @@ class MoviesController extends Controller
     // =============================================================================
     public function get($id = null): array
     {
-        if ($this->is_set($id)) {
+
+        if (parent::is_set($id)) {
             return $this->getById($id);
         } else {
             return $this->getAll();
         }
     }
 
-    // idで抽出
-    private function getById($id): array
-    {
-        $res_movie  = parent::selectById(Sql::SelectMoviesById, $id)[0];    // 映画TBL検索
-        $res_types  = parent::selectById(Sql::SelectMovieTypesById, $id);   // 上映種別TBL検索
-        $res_images = parent::selectById(Sql::SelectImagesById, $id);       // 画像TBL検索
-
-        // 映画情報と上映種別情報と画像情報の連結
-        $res_movie["types"]     = array();
-        $res_movie["images"]    = array();
-
-        foreach ($res_types as $res_type) {
-            array_push($res_movie["types"], $res_type['name']);
-        }
-
-        foreach ($res_images as $res_image) {
-            array_push($res_movie["images"], parent::url_image($res_image['image_url']));
-        }
-
-        if ($res_movie) {
-            return $res_movie;
-        } else {
-            $this->code = 500;
-            return ["error" => ["type" => "fatal_error"]];
-        }
-    }
-
-    // すべて
+    // 映画リストの取得
     private function getAll(): array
     {
-        $res_movies = parent::select(Sql::SelectMoviesAll);     // 映画TBL検索
-        $res_types  = parent::select(Sql::SelectMovieTypesAll); // 上映種別TBL検索
-        $res_images = parent::select(Sql::SelectImagesAll);     // 画像TBL検索
+        $res_movies = parent::select(Sql::SelectMoviesAll);         // 映画TBL検索
+        $res_types  = parent::select(Sql::SelectMovieTypesAll);     // 上映種別TBL検索
 
-        // 映画情報と上映種別情報と画像情報の連結
+        // 映画情報と上映種別情報の連結
         $cnt = 0;
         while (count($res_movies) > $cnt) {
             $res_movies[$cnt]["types"] = array();
-            $res_movies[$cnt]["images"] = array();
 
             foreach ($res_types as $res_type) {
                 if ($res_movies[$cnt]["id"] == $res_type["id"]) {
@@ -60,14 +32,11 @@ class MoviesController extends Controller
                 }
             }
 
-            foreach ($res_images as $res_image) {
-                if ($res_movies[$cnt]["id"] == $res_image["id"]) {
-                    array_push($res_movies[$cnt]["images"], $this->url_image($res_image["image_url"]));
-                }
-            }
+            $res_movies[$cnt]["thumbnail"] = parent::url_image($res_movies[$cnt]["thumbnail"]);
 
             $cnt++;
         }
+
 
         if ($res_movies) {
             return $res_movies;
@@ -75,5 +44,60 @@ class MoviesController extends Controller
             $this->code = 500;
             return ["error" => ["type" => "fatal_error"]];
         }
+    }
+
+    // 映画
+    private function getById($id): array
+    {
+        // 映画情報検索
+        $res_movies         = parent::selectById(Sql::SelectMoviesById, $id);
+        $res_movie_types    = parent::selectById(Sql::SelectMovieTypesById, $id);
+        $res_movie_images   = parent::selectById(Sql::SelectImagesById, $id);
+
+        // 上映管理検索
+        $res_manages        = $this->selectSchedulesById($id);
+        $cnt = 0;
+        while (count($res_manages) > $cnt) {
+            // 上映種別TBL検索
+            $res_manages[$cnt]["types"] = array();
+            $res_manage_types  = parent::selectById(Sql::SelectManageTypesById, $res_manages[$cnt]["id"]);
+
+            foreach ($res_manage_types as $type) {
+                array_push($res_manages[$cnt]["types"], $type["name"]);
+            }
+
+            $cnt++;
+        }
+
+        $res_movies["images"] = array();
+        foreach ($res_movie_images as $image) {
+            array_push($res_movies["images"], parent::url_image($image["image_url"]));
+        }
+
+        $res_movies["type"] = array();
+        foreach ($res_movie_types as $type) {
+            array_push($res_movies["type"], $type["name"]);
+        }
+
+
+        $res_movies["manages"] = $res_manages;
+
+        if ($res_movies) {
+            return $res_movies;
+        } else {
+            $this->code = 500;
+            return ["error" => ["type" => "fatal_error"]];
+        }
+    }
+
+    // スケジュール検索
+    private function selectSchedulesById($id): array
+    {
+        $sourses = $this->db->connect()->prepare(Sql::SelectSchedulesById);
+        $sourses->bindValue(":time1", Env::AD_TIME, PDO::PARAM_INT);
+        $sourses->bindValue(":time2", Env::AD_TIME, PDO::PARAM_INT);
+        $sourses->bindValue(":id", $id, PDO::PARAM_INT);
+        $sourses->execute();
+        return $sourses->fetchAll(PDO::FETCH_ASSOC);
     }
 }
